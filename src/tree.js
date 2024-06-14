d1671c319fb7eaa9b8e15eedc237b4058ca2a8a1
Herbert-Haase <herberthaase02@gmx.de>
2024-06-14 18:37:29 +0200

    add value insertion

--- src/tree.js
+++ src/tree.js
@@ -7,16 +7,18 @@ class Tree {
     this.root = this.buildtree(this.sortedArr);
   }
   buildtree(arr) {
-    if (arr.length < 2) {
-      return new Node(arr[0], null, null);
+    if (arr.length === 1) {
+      return new Node(arr[0]);
+    } else if (arr.length === 2) {
+      return new Node(arr[0], null, new Node(arr[1]));
     }
     const start = 0;
     const end = arr.length - 1;
-    const middle = (start + end) / 2;
-    const leftArr = arr.slice(0, middle);
-    const rightArr = arr.slice(middle + 1);
+    const middleIndex = Math.floor((start + end) / 2);
+    const leftArr = arr.slice(0, middleIndex);
+    const rightArr = arr.slice(middleIndex + 1);
     return new Node(
-      arr[middle],
+      arr[middleIndex],
       this.buildtree(leftArr),
       this.buildtree(rightArr)
     );
@@ -26,6 +28,26 @@ class Tree {
   }
   sortNumbers(arr) {
     return arr.sort((a, b) => a - b);
+  }
+  checkForDuplicates(value) {
+    return this.arr.includes(value);
+  }
+  insert(value, node = this.root) {
+    if (!this.checkForDuplicates(value)) {
+      if (value < node.value) {
+        if (node.left === null) {
+          node.left = new Node(value);
+        } else {
+          this.insert(value, node.left);
+        }
+      } else {
+        if (node.right === null) {
+          node.right = new Node(value);
+        } else {
+          this.insert(value, node.right);
+        }
+      }
+    }
   }
 }
 
